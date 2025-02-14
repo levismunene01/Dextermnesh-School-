ff5f6eb7003d9ba1beedcf59339e56af11d26ec6
levis <levismunene01@gmail.com>
2025-02-14 09:46:59 +0300

    Initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,73 @@
 // src/components/Header.js
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/Header.css'
 
 function Header() {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          {/* <li><Link to="/about">About Us</Link></li>
-          <li><Link to="/courses">Courses</Link></li>
-          <li><Link to="/faculty">Faculty</Link></li>
-          <li><Link to="/admissions">Admissions</Link></li>
-          <li><Link to="/contact">Contact Us</Link></li> */}
-        </ul>
+    <header className="custom-header">
+      <nav className="navbar navbar-expand-lg">
+        <div className="container d-flex justify-content-between align-items-center">
+          <Link className="navbar-brand logo-text" to="/">
+            Namibia school
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarContent"
+            aria-controls="navbarContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div className="collapse navbar-collapse justify-content-between align-items-center" id="navbarContent">
+            <ul className="navbar-nav d-flex flex-row gap-3 mb-0">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/events">Events</Link>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  More
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><Link className="dropdown-item" to="/about">About</Link></li>
+                  <li><Link className="dropdown-item" to="/contact">Contact</Link></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><Link className="dropdown-item" to="/faq">FAQ</Link></li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <span className="nav-link disabled" tabIndex="-1" aria-disabled="true">Coming Soon</span>
+              </li>
+            </ul>
+
+            <form className="d-flex search-form align-items-center ms-3">
+              <input
+                className="form-control search-input"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn search-btn" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
       </nav>
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React from 'react'
+import '../styles/HomePage.css' // You'll create this next
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to Our School</h1>
-      <p>Discover our programs, meet our faculty, and learn about our admissions process.</p>
-    </div>
-  );
-};
+    <div className="homepage">
+      <section className="hero">
+        <h1>Welcome to Our Namibia School</h1>
+        <p>Discover our programs, meet our faculty, and learn about our admissions process.</p>
+      </section>
 
-export default HomePage;
+      <section className="intro">
+        <h2>
+          We are a private CBC/8-4-4 school in Kenya with 2 branches across the cities of Nairobi and Mombasa. Our 25 years of service has seen us reach so many academic heights and success. We take pride in our unique offering that presents learners and teachers with an experience that is rare to find in many high schools.
+        </h2>
+      </section>
+
+      <section className="model">
+        <h3>OUR EDUCATION MODEL</h3>
+        <h1>A MODERN SCHOOL</h1>
+        <p>
+          We do understand the inevitable fact that as each day passes by, the world is being taken over by technology. It is for this reason that we prefer to incorporate it into our students’ education. We have adopted various learning management systems such as Coolsis, Google Suite for Education, and Zoom.
+          <br /><br />
+          These software are meant to complement physical classes by offering platforms for teachers to assign homework and tests to the students, as well as enabling the teachers to come up with fun quizzes which help stimulate the students in class.
+          <br /><br />
+          Parents can track their students’ educational progress as well as their discipline since their academic grades and discipline reports are made available in the systems.
+          <br /><br />
+          During the COVID 19 pandemic, for instance, many students all over the world were unfortunate to stay away from school since many countries enforced a mandatory lockdown. Our students however were not disadvantaged since it was easy for us to transition to a fully online education model. All our staff are technology savvy and our goal is to make sure that every student is very computer literate by the time they leave this place. They will need that dearly in today’s world.
+        </p>
+      </section>
+
+      <section className="excellence">
+        <h3>ACCESS TO SUCCESS</h3>
+        <h1>ACADEMIC EXCELLENCE</h1>
+        <p>
+          We do understand the inevitable fact that as each day passes by, the world is being taken over by technology. It is for this reason that we prefer to incorporate it into our students’ education. We have adopted various learning management systems such as Coolsis, Google Suite for Education, and Zoom.
+          <br /><br />
+          These software are meant to complement physical classes by offering platforms for teachers to assign homework and tests to the students, as well as enabling the teachers to come up with fun quizzes which help stimulate the students in class.
+          <br /><br />
+          Parents can track their students’ educational progress as well as their discipline since their academic grades and discipline reports are made available in the systems.
+          <br /><br />
+          During the COVID 19 pandemic, for instance, many students all over the world were unfortunate to stay away from school since many countries enforced a mandatory lockdown. Our students however were not disadvantaged since it was easy for us to transition to a fully online education model. All our staff are technology savvy and our goal is to make sure that every student is very computer literate by the time they leave this place. They will need that dearly in today’s world.
+        </p>
+      </section>
+    </div>
+  )
+}
+
+export default HomePage
